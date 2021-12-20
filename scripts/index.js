cec4c7a74640774836a946dce9239d1c858ae125
RobertoHortaFilho <88946776+RobertoHortaFilho@users.noreply.github.com>
2021-12-20 13:57:29 -0300

    commit pagina do anime
    criando a pagina do anime com informaçoes

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,21 @@
 import { requestA, requestTop} from './request.js'
+import { addAnime } from './add-anime.js'
+
+//Variaveis de controle
+var busca = {
+    Lastlink: "",
+    pagina: 1,
+    tipo: 'results' ,
+    req: true,
+}
+
+// reset pagina
+function resetPagina(){
+    busca.pagina = 1
+    document.querySelector('.pagina').innerText = "- 1 -"
+    busca.req = true;
+
+}
 
 // receber valor form
 let botao = document.querySelector('.busca-butao')
@@ -7,14 +24,21 @@ botao.addEventListener('click', (e) =>{
 
     let valor_busca = document.querySelector('.busca-texto').value
     console.log(valor_busca)
-    requestA(valor_busca)
+    let res = requestA(valor_busca)
+    busca.Lastlink = res.link
+    busca.tipo = res.tipo
+    resetPagina()
+    
 })
 
 //carregar a pagina a primeira vez com escolhas predifinidas
 let opcoes = ['monogatari','hanako','boku no hero','otakoi','bananafish']
 let escolha = Math.ceil((Math.random() * 4))
-console.log(opcoes[escolha])
-requestA(opcoes[escolha])
+//console.log(opcoes[escolha])
+let res = requestA(opcoes[escolha])
+busca.Lastlink = res.link
+busca.tipo = res.tipo
+resetPagina()
 
 
 
@@ -22,7 +46,7 @@ requestA(opcoes[escolha])
 let buttons = document.querySelectorAll('.botao-genero')
 buttons.forEach( (data)=>{
     let link
-    let num = 2
+    let num = 1
     switch(data.id){
         case 'topa':
             link = 'https://api.jikan.moe/v3/top/anime'
@@ -34,17 +58,96 @@ buttons.forEach( (data)=>{
             break;
         case 'season':
             link = 'https://api.jikan.moe/v3/season/2021/winter'
-            num = 1
             break;
         case 'action':
             link = 'https://api.jikan.moe/v3/genre/anime/1'
             break;
+        case 'adventure':
+            link ='https://api.jikan.moe/v3/genre/anime/2'
+            break;
+        case 'comedy':
+            link ='https://api.jikan.moe/v3/genre/anime/4'
+            break;
+        case 'drama':
+            link ='https://api.jikan.moe/v3/genre/anime/8'
+            break;
+        case 'fantasy':
+            link ='https://api.jikan.moe/v3/genre/anime/10'
+            break;
+        case 'sifi':
+            link ='https://api.jikan.moe/v3/genre/anime/24'
+            break;
+        case 'sport':
+            link ='https://api.jikan.moe/v3/genre/anime/30'
+            break;
+                
     }
     data.addEventListener('click', (buscar) =>{
-        requestTop(link,num)
+        let res = requestTop(link,num)
+        busca.Lastlink = res.link
+        busca.tipo = res.tipo
+        resetPagina()
     })
 })
 // 1 acao 2 aventura 4 comedy 8 drama 10 fantasia 24 sifi 30 sport 
+
+//passar e voltar paginas
+const backP = document.querySelector('.back')
+backP.addEventListener('click', ()=>{
+    if(busca.req){
+        if (busca.pagina > 1){
+            busca.req = false
+            busca.pagina --;
+            requestPagina(busca.Lastlink,busca.pagina,busca.tipo)
+            document.querySelector('.pagina').innerText = `- ${busca.pagina} -`
+        }
+    }
+})
+
+const skipP = document.querySelector('.skip')
+skipP.addEventListener('click',()=>{
+    if (busca.req){
+        if (busca.pagina < 500){
+            busca.req = false
+            busca.pagina ++;
+            requestPagina(busca.Lastlink,busca.pagina,busca.tipo)
+            document.querySelector('.pagina').innerText = `- ${busca.pagina} -`
+        }
+    }
+})
+
+// previnir mts requests
+function requestPagina(Llink,pag,tipo){
+    fetch(`${Llink}/${pag}`).then( response => {
+        response.json().then(data =>{
+            document.querySelector('.animesreq').innerHTML = ""
+            switch (tipo){
+                case 'anime':
+                    var animes = data.anime
+                    break;
+                case 'top':
+                    var animes = data.top
+                    break;
+                case 'results':
+                    var animes = data.results
+                    break;
+            }
+            animes.forEach(element => {
+                let texto = ''
+                if (element.title.length > 40){
+                    texto = `${element.title.substring(0,30)}...`
+                }else{
+                    texto = element.title
+                }
+                addAnime(texto,element.image_url)
+                
+            })
+            console.log('animes add pag: '+pag)
+            console.log(`${Llink}/${pag}`)
+            busca.req = true;
+        })
+    })
+}
 
 
 
@@ -57,6 +160,7 @@ setInterval(()=>{
     }else{
         animes_week.style.height = '74%'
     }
+    //console.log(busca)
 }, 500)
 
 
--- scripts/request.js
+++ scripts/request.js
@@ -1,7 +1,7 @@
 import { addAnime } from './add-anime.js'
 const link = 'https://api.jikan.moe/v3/search/anime?q='
 
-// 1 acao 2 aventura 4 comedy 8 drama 10 fantasia 24 sifi 30 sport 
+
 export function requestA(name){
     document.querySelector('.animesreq').innerHTML = ""
     fetch(link+name).then( response => {
@@ -24,14 +24,23 @@ export function requestA(name){
         }).catch( error => console.log(error))
     }).catch( err => console.log( err ))
 
+    return {
+        link:link+name,
+        tipo:'results'
+    }
 }
 
 export function requestTop(link2,num){
+    let voltar = {
+        link: link2,
+        tipo: (num == 0) ? 'top' : 'anime'
+    }
     fetch(link2).then( response => {
         response.json().then( data => {
             document.querySelector('.animesreq').innerHTML = ""
-                console.log(data)
+                //console.log(data)
                 let animes
+                
                 switch(num){
                     case 0:
                         animes = data.top
@@ -39,6 +48,9 @@ export function requestTop(link2,num){
                     case 1:
                         animes = data.anime
                         break;
+
+
+                 
                 }
                 animes.forEach(element => {
                     let texto = ''
@@ -52,5 +64,5 @@ export function requestTop(link2,num){
                 
         }).catch( error => console.log(error))}
     ).catch( error => console.log(error))
-
+        return voltar
 }
